438a64c0c5fe18b56ea28f6b58333e928ef34e05
Leo <103922926@qq.com>
2020-12-26 10:51:07 +0000

    install libbpf in staging dir

--- package/libbpf-bootstrap/libbpf-bootstrap.mk
+++ package/libbpf-bootstrap/libbpf-bootstrap.mk
@@ -9,17 +9,16 @@ LIBBPF_BOOTSTRAP_SITE = $(call github,libbpf,libbpf-bootstrap,$(LIBBPF_BOOTSTRAP
 LIBBPF_BOOTSTRAP_LICENSE = LGPL-2.1 or BSD-2-Clause
 LIBBPF_BOOTSTRAP_LICENSE_FILES = LICENSE
 LIBBPF_BOOTSTRAP_DEPENDENCIES = libbpf
-LIBBPF_BOOTSTRAP_CFLAGS = "-g -O2 -Werror -Wall -I$(TARGET_DIR)/usr/include"
-LIBBPF_BOOTSTRAP_INSTALL_PREFIX = "$(TARGET_DIR)/usr"
+LIBBPF_BOOTSTRAP_CFLAGS = "-g -O2 -Werror -Wall -I$(STAGING_DIR)/usr/include"
 
 LIBBPF_BOOTSTRAP_ENV = \
 		CC=$(TARGET_CC)  \
 		CLANG=clang-10  \
 		LLVM_STRIP=llvm-strip-10  \
 		LIBBPF_SRC="$(BUILD_DIR)/libbpf-$(LIBBPF_VERSION)/src"  \
-		LIBBPF_OBJ="$(TARGET_DIR)/usr/lib/libbpf.a"  \
+		LIBBPF_OBJ="$(STAGING_DIR)/usr/lib/libbpf.a"  \
 		ARCH=$(BR2_ARCH)\
-		INCLUDES="-I$(@D)/src/.output -I$(TARGET_DIR)/usr/include -I$(TARGET_DIR)/usr/include/uapi"
+		INCLUDES="-I$(@D)/src/.output -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/uapi"
 
 define LIBBPF_BOOTSTRAP_BUILD_CMDS
 	$(TARGET_MAKE_ENV)  $(MAKE) -C $(@D)/src $(LIBBPF_BOOTSTRAP_ENV)
